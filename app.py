49b34776a43c6588f2154510ba8214fb9447a62a
Prasoon <39785096+it5prasoon@users.noreply.github.com>
2020-09-18 18:51:38 +0530

    some frontend changes by prasoon

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask,request, url_for, redirect, render_template
+from flask import Flask, request, url_for, redirect, render_template
 import pickle
 import numpy as np
 
 app = Flask(__name__)
 
-model=pickle.load(open('model.pkl','rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 
 @app.route('/')
@@ -12,19 +12,23 @@ def hello_world():
     return render_template("titanic.html")
 
 
-@app.route('/predict',methods=['POST','GET'])
+@app.route('/predict', methods=['POST', 'GET'])
 def predict():
-    int_features=[int(x) for x in request.form.values()]
-    final=[np.array(int_features)]
+    int_features = [int(x) for x in request.form.values()]
+    final = [np.array(int_features)]
     print(int_features)
     print(final)
-    prediction=model.predict_proba(final)
-    output='{0:.{1}f}'.format(prediction[0][1], 2)
+    prediction = model.predict_proba(final)
+    output = '{0:.{1}f}'.format(prediction[0][1], 2)
 
-    if output>str(0.5):
-        return render_template('titanic.html',pred='Your life is not in Danger.\nProbability of survival is {}'.format(output),bhai="you are safe!")
+    if output > str(0.5):
+        return render_template('titanic.html',
+                               pred='<p>Your life is not in Danger.\nProbability of survival is {}</p>'.format(output),
+                               bhai="you are safe!")
     else:
-        return render_template('titanic.html',pred='Your life is in danger.\n Probability of survival is {}'.format(output),bhai="bye bye")
+        return render_template('titanic.html',
+                               pred='Your life is in danger.\n Probability of survival is {}'.format(output),
+                               bhai="bye bye")
 
 
 if __name__ == '__main__':
